b9f119b92ea960e13bfc96eb4901f4a71cb6edbf
Judah <syluxprime40@gmail.com>
2019-06-10 18:09:49 -0600

    test

--- Assets/AvoidGame/WeaponSlot.cs
+++ Assets/AvoidGame/WeaponSlot.cs
@@ -7,9 +7,12 @@ public class WeaponSlot : MonoBehaviour
 
     public List<Transform> Slots;
     public GameObject Projectile;
+    AudioSource audiosrc;
 
     public void Fire()
     {
+        audiosrc = GetComponent<AudioSource>();
+        audiosrc.Play();
         Slots.ForEach(slot =>
        {
            Instantiate(Projectile,
